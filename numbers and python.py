16f512e464cd8bc9cc3f10f0b70c71ecabc3c8b0
Nishit Prajapati <acmnishit@gmail.com>
2022-05-11 09:36:37 +0530

    Update numbers and python.py

--- numbers and python.py
+++ numbers and python.py
@@ -8,3 +8,16 @@ while k > 0:
   k = int(k/10)
 print(sum)
 ########################################################################################
+def inspect(x):
+  '''this function describes numbers
+  whether it is positive or negative
+  or zero or none of them'''
+    if x == 0:
+        print(x, "is zero")
+    elif x > 0:
+        print(x, "is positive")
+    elif x < 0:
+        print(x, "is negative")
+    else:
+        print(x, "is unlike anything I've ever seen...")
+ ########################################################################################
